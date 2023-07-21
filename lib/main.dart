3f9bfc39a21a6f42d76e824c95cbfd69bce60eef
Hudson Finn <hudson.finn@zohomail.com>
2023-07-21 11:38:14 -0400

    Add project

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Home(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key, required title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: const Text('Welcome home!'),
+    );
+  }
+}
